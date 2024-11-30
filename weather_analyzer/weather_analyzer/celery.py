266d1258e26aaf3603e6b2d836cb04bd71a20d3e
BGabor01 <busnes14@gmail.com>
2024-11-30 19:51:09 +0100

    celery: Fixed autodiscover

--- weather_analyzer/weather_analyzer/celery.py
+++ weather_analyzer/weather_analyzer/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weather_analyzer.settings")
 
@@ -8,4 +9,4 @@ app = Celery("weather_analyzer")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
-app.autodiscover_tasks(["weather_api.tasks"])
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
