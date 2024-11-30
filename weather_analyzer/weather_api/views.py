7c3e6eb931b3b868ab44a07236194b611390b6f1
BGabor01 <busnes14@gmail.com>
2024-11-30 21:47:06 +0100

    views: API endpoints

--- weather_analyzer/weather_api/views.py
+++ weather_analyzer/weather_api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from django_filters import rest_framework as filters
 
-# Create your views here.
+from .models import City, WeatherData
+from .serializers import CitySerializer, HistoricalDataSerializer
+from .filters import CityFilter, HistoricalDataFilter
+
+
+class ListCitiesView(ListAPIView):
+    serializer_class = CitySerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = CityFilter
+    queryset = City.objects.all()
+
+
+class ListHistoricalDataForCity(ListAPIView):
+    serializer_class = HistoricalDataSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = HistoricalDataFilter
+    queryset = WeatherData.objects.all()
